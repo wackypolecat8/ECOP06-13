d8fd6274d61534de97b83edd134ff0bcb578f4a6
wackypolecat8 <146854286+wackypolecat8@users.noreply.github.com>
2023-10-23 11:53:05 +0000

    Atualizando tudo

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+import pandas as pd
+import matplotlib.pyplot as pit
+
+st.set_page_config('Gabriel - ECOP06',
+                    'https://unifei.edu.br/wp-content/themes/twentytwelve-child/img/cabecalho/logo-unifei-oficial.png'
+
+st.title('Página Demo ECOP06')
+
+esportes = pd.read_csv('https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv')
+
+st.dataframe(esportes)
+
+
